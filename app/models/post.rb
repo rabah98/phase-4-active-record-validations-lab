2bbd07a5310ff94e8faae851cfee00eec1c905f0
Rabah <rmerzoug471@gmail.com>
2022-09-04 14:48:56 -0500

    commit tree 782ceb9b0b96437bf8b5a841e9909ba078adb4fd

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,16 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: { minimum: 250}
+    validates :summary, length: { maximum: 250}
+    validates :category, inclusion: { in: ["Fiction", "Non-Fiction"],
+        message: "category is not a valid"
+    }
+    validate :title_is_sufficiently_clickbait_y
+
+    def title_is_sufficiently_clickbait_y
+        clickbait = ["Won't Believe", "Secret", "Top [number]", "Guess"]
+        unless clickbait.include?(title)
+            errors.add(:title, "the title is not sufficiently clickbait-y!")
+          end
+    end
 end
